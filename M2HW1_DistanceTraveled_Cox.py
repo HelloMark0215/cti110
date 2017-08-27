8332fa83ea19833b3dfc568ace4a771fd99552f7
HelloMark0215 <31361936+HelloMark0215@users.noreply.github.com>
2017-08-26 21:52:14 -0400

    This was my second program

---
+++ M2HW1_DistanceTraveled_Cox.py
@@ -0,0 +1,41 @@
+#CTI-110
+#M2H1 - Distance Traveled
+#William Cox
+#26 August 2017
+
+# Assuming no delays, the distance that a car travels down the interstate can be calculated with the following formula:
+# Distance = Speed X Time
+
+print ("Hello World")
+
+print ("This program is calculating miles traveled by an automobile traveling at a set speed for a certain number of hours")
+
+#variable one is the speed traveled
+
+speed = 70
+
+# Variable two is distance after 6 hours
+
+DistanceAfter6 = speed * 6
+
+# Variable three is distance ater 10 hours
+
+distanceAfter10 = speed * 10
+
+# Variable four is distance after 15 hours
+
+distanceAfter15 = speed * 15
+
+# Distance after 6 hours
+print ( "The number of miles driven by a car traveling 70MPH for 6 hours is", DistanceAfter6 )
+
+# Distance after 10 hours
+print ( "The number of miles driven by a car traveling 70MPH for 10 hours is", distanceAfter10 )
+
+# Distance after 10 hours
+print ( "The number of miles driven by a car traveling 70MPH for 15 hours is", distanceAfter15 )
+
+
+
+
+
