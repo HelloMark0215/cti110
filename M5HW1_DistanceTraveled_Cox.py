d0ffad68d8cf76dd4e8fd26df23173ecf09721ca
HelloMark0215 <31361936+HelloMark0215@users.noreply.github.com>
2017-10-13 21:21:10 -0400

    Add files via upload
    This assignment created a loop to show the distance traveled each hour

---
+++ M5HW1_DistanceTraveled_Cox.py
@@ -0,0 +1,34 @@
+#CTI-110
+#M5H1 - Distance Traveled Loop
+#William Cox
+#13 October 2017
+
+
+def main():
+
+# Assuming no delays, the distance that a car travels down the interstate can be calculated with the following formula:
+# Distance = Speed * Time
+
+#variable one is the speed traveled
+    speed = int(input( "What is the speed of the vehicle in MPH? "))
+
+# Variable two is number of hours traveled
+    hourstraveled = int(input("How many hours has it traveled? "))
+
+
+# I am starting with 1 hour becasue at 0 hours the distance would be 0 miles
+    hours = 1
+
+# This variable is required for the inital value of the loop
+    distance = speed * hours
+
+    print( '\n' + "Hour" + '\t' + " Distance Traveled" + '\n' + "-------------------------" )  
+
+    while hours <= hourstraveled:
+        print (hours , '\t' ,  distance,)
+        hours = hours + 1
+        distance = speed * hours
+    
+main()
+    
+    
