1b488b089b6d61392ebf771074da617a03279421
HelloMark0215 <31361936+HelloMark0215@users.noreply.github.com>
2017-11-05 13:23:24 -0500

    very basic snowflake using turtle graphics

---
+++ M5Lab3_Cox.py
@@ -0,0 +1,15 @@
+#CTI-110
+#M5Lab3
+#William Cox
+#22 October 2017
+
+import turtle
+turtle.shape("turtle")
+for i in range(10):
+    for i in range(2):
+        turtle.forward(100)
+        turtle.right(60)
+        turtle.forward(100)
+        turtle.right(120)
+    turtle.right(36)
+    
